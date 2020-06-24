6e56ba062a48c9a9ce475aab6dac7fbe5aa6ab4d
kevinngth <kevin.ng.th@gmail.com>
2020-06-25 00:55:35 +0800

    refactored 2 helper functions

--- src/matcher.js
+++ src/matcher.js
@@ -1,7 +1,35 @@
-const matcher = (input) => {
-    const output = "c1 j4\nc2 j2\nc3 j1\nc4 j3";
-    console.log(output);
+const inputHandler = (input) => {
+    // split into jobs and candidates
+    const level1 = input.split("\n\n");
+    // split into each individual job and candidate
+    const output = level1.map((e) => {
+        const level2 = e.split("\n");
+        const level3 = level2.map((level4) => {
+            // split "j1: c3 c4 c2 c1" into ["j1", "c3 c4 c2 c1"]
+            const [a, level5] = level4.split(": ");
+            // split "c3 c4 c2 c1" into [ 'c3', 'c4', 'c2', 'c1' ]
+            console.log(level5);
+            return level5.split(" ");
+        });
+        return level3;
+    });
     return output;
 };
 
-module.exports = matcher;
+const outputHandler = (twoDArray) => {
+    const arrOfCanJobPairs = twoDArray.map((candidate) => {
+        return candidate.join(" ");
+    });
+    const output = arrOfCanJobPairs.join("\n");
+    return output;
+};
+
+const matcher = (input) => {
+    const [jobs, candidates] = inputHandler(input);
+    // console.log(jobs);
+    // console.log(candidates);
+    const output = "c1 j4\nc2 j2\nc3 j1\nc4 j3";
+    return output;
+};
+
+module.exports = { matcher, inputHandler, outputHandler };
--- test/matcher.test.js
+++ test/matcher.test.js
@@ -1,12 +1,46 @@
-const matcher = require("../src/matcher");
-
-test("should return a string", () => {
-    expect(typeof matcher("Hello")).toBe("string");
-});
+const { matcher, inputHandler, outputHandler } = require("../src/matcher");
 
 test("should pass test case 1", () => {
     const input =
         "j1: c3 c4 c2 c1\nj2: c2 c3 c1 c4\nj3: c4 c2 c3 c1\nj4: c4 c3 c1 c2\n\nc1: j4 j1 j2 j3\nc2: j1 j2 j4 j3\nc3: j1 j3 j4 j2\nc4: j1 j3 j4 j2";
     const output = "c1 j4\nc2 j2\nc3 j1\nc4 j3";
     expect(matcher(input)).toBe(output);
+});
+
+test("should split input into arrays", () => {
+    const input =
+        "j1: c3 c4 c2 c1\nj2: c2 c3 c1 c4\nj3: c4 c2 c3 c1\nj4: c4 c3 c1 c2\n\nc1: j4 j1 j2 j3\nc2: j1 j2 j4 j3\nc3: j1 j3 j4 j2\nc4: j1 j3 j4 j2";
+    const output = [
+        [
+            ["c3", "c4", "c2", "c1"],
+            ["c2", "c3", "c1", "c4"],
+            ["c4", "c2", "c3", "c1"],
+            ["c4", "c3", "c1", "c2"],
+        ],
+        [
+            ["j4", "j1", "j2", "j3"],
+            ["j1", "j2", "j4", "j3"],
+            ["j1", "j3", "j4", "j2"],
+            ["j1", "j3", "j4", "j2"],
+        ],
+    ];
+    expect(inputHandler(input)[0][0]).toEqual(output[0][0]);
+    expect(inputHandler(input)[0][1]).toEqual(output[0][1]);
+    expect(inputHandler(input)[0][2]).toEqual(output[0][2]);
+    expect(inputHandler(input)[0][3]).toEqual(output[0][3]);
+    expect(inputHandler(input)[1][0]).toEqual(output[1][0]);
+    expect(inputHandler(input)[1][1]).toEqual(output[1][1]);
+    expect(inputHandler(input)[1][2]).toEqual(output[1][2]);
+    expect(inputHandler(input)[1][3]).toEqual(output[1][3]);
+});
+
+test("should format output", () => {
+    input = [
+        ["c1", "j4"],
+        ["c2", "j2"],
+        ["c3", "j1"],
+        ["c4", "j3"],
+    ];
+    const output = "c1 j4\nc2 j2\nc3 j1\nc4 j3";
+    expect(outputHandler(input)).toBe(output);
 });
