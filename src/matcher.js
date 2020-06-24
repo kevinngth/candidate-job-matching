ee8441cc00c2c44b2a100bb8683d482b8c31c5a5
kevinngth <kevin.ng.th@gmail.com>
2020-06-25 01:06:25 +0800

    destructured input

--- src/matcher.js
+++ src/matcher.js
@@ -8,7 +8,6 @@ const inputHandler = (input) => {
             // split "j1: c3 c4 c2 c1" into ["j1", "c3 c4 c2 c1"]
             const [a, level5] = level4.split(": ");
             // split "c3 c4 c2 c1" into [ 'c3', 'c4', 'c2', 'c1' ]
-            console.log(level5);
             return level5.split(" ");
         });
         return level3;
@@ -26,8 +25,12 @@ const outputHandler = (twoDArray) => {
 
 const matcher = (input) => {
     const [jobs, candidates] = inputHandler(input);
-    // console.log(jobs);
-    // console.log(candidates);
+    const result = [];
+    candidates.forEach((candidate, i) => {
+        const job = candidate[0];
+        result.push(["c" + (i + 1), job]);
+    });
+    console.log(result);
     const output = "c1 j4\nc2 j2\nc3 j1\nc4 j3";
     return output;
 };
